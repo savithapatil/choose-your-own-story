929e1e3569c218cd5fca318fabca566373dcef7f
barrierwill <72474580+barrierwill@users.noreply.github.com>
2020-11-23 16:22:11 -0500

    pics

---
+++ achievements.js
@@ -0,0 +1,15 @@
+//Code to open achievement page
+const achievements = document.querySelector('#achievements');
+achievements.addEventListener('click', (e) => {
+    console.log("Made it here");
+    e.preventDefault();
+        window.location.href = "achievements.html"
+        ("#alex").removeClass(grey);
+});
+
+//Code to save the html page to back to game (Optional)
+("#alex").removeClass(grey);
+// Code to edit class so its no longer grey
+console.log("Did this");
+
+//Code to actually enable achievement as true
